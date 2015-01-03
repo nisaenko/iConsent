53d26cd24c203649e56c711d68bea277fd325097
nisaenko <nisaenko@users.noreply.github.com>
2015-01-02 21:53:16 -0500

    angular integration fixed. not it is up and running

--- server.js
+++ server.js
@@ -4,14 +4,14 @@
 var express = require('express');
     app = express();
 
-app.get('/js', function(req, res){
-    res.sendFile(__dirname + '/client/js');
-});
+
+
+app.use(express.static('bower_components'));
+app.use(express.static('client'));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/client/views/index.html');
 });
-
 
 app.listen(3000, function () {
     console.log("iConsent Server is listening port 3000");
